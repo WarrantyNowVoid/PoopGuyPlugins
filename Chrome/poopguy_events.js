6076de412728448674711cb1f0415371994bb216
Nick Pettazzoni <pettazz@gmail.com>
2013-12-05 14:17:26 -0500

    remove unnecessary logging

--- Chrome/poopguy_events.js
+++ Chrome/poopguy_events.js
@@ -1,11 +1,11 @@
 chrome.browserAction.onClicked.addListener(function(tab){
     chrome.tabs.executeScript({
-        code: 'console.log("click"); startPooping();'
+        code: 'startPooping();'
     });
 });
 
 chrome.omnibox.onInputEntered.addListener(function(text, disposition){
     chrome.tabs.executeScript({
-        code: 'console.log("type"); startPooping();'
+        code: 'startPooping();'
     });
 });
